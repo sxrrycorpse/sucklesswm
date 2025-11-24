a018bd7e940afaf07bfc4f059ad28e98325434ef
sxrrycorpse <114552693+sxrrycorpse@users.noreply.github.com>
2025-11-24 13:13:43 +0530

    update zoom modkey

--- dwm/config.h
+++ dwm/config.h
@@ -88,7 +88,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_x,      togglebar,      {0} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_z,      zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
