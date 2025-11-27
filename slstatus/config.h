8477dfb8693848ec28188b50a3e220e867863b1d
sxrrycorpse <114552693+sxrrycorpse@users.noreply.github.com>
2025-11-27 18:55:57 +0530

    add ram_used/slstatus

--- slstatus/config.h
+++ slstatus/config.h
@@ -66,5 +66,6 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
+	{ ram_used, "%s | ",        "NULL" },
 	{ datetime, "%s",           "%a %b %d %Y | %H:%M" },
 };
